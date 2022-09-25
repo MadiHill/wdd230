e942b11ed2c625213b31a472ac4c63d1891283e0
madihill <hil19012@byui.edu>
2022-09-24 18:40:40 -0600

    committing all changes

---
+++ lesson2/script/main.js
@@ -0,0 +1,15 @@
+let date = new Date();
+
+console.log(date);
+
+let currentdate = document.lastModified;
+
+console.log(currentdate);
+
+document.querySelector('div').textContent = ('Last Updated: ' + currentdate);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+document.querySelector('.year').textContent = year;
