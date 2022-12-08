563b2c794564782e3e2b5da61f0fdcd3eb42d021
madihill <hil19012@byui.edu>
2022-12-07 21:04:23 -0700

    commit for all

--- lesson5/bom.js
+++ lesson5/bom.js
@@ -1,12 +1,12 @@
-const input = document.querySelector('#chapter')
+const input = document.querySelector('#favchap')
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('.list');
 
 
 button.addEventListener('click',addButton)
 
 function addButton() {
-    // Create: An li element, delete button,input
+    
  let listItem = document.createElement('li')
  let listItemButton = document.createElement('button')
  let listItemText = document.createElement('p')
@@ -15,7 +15,7 @@ function addButton() {
  listItem.appendChild(listItemText)
  listItem.appendChild(listItemButton)
  listItemText.innerHTML = listItemContent
- listItemButton.innerHTML = 'X'
+ listItemButton.innerHTML = '❌'
  listItemButton.id = 'list-button'
  listItemButton.addEventListener('click',removeItems)
  input.value = '';
